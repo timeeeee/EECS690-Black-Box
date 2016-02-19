c376b82b04ac90c2b500c31f0597e03a7c67c4c2
DSoliz <zilosgodi@gmail.com>
2016-02-19 16:22:46 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,11 @@
 //   FreeRTOS
 
 // Declare globals
+extern double v_temp;  //measured temperature voltage
+extern double dig_temp; // the digital representation of the voltage)
+extern double conv_temp; //the measured temperature in actual degrees (celsius?)
+extern double set_temp;  //the goal temperature
+extern double pid_out;  //the PID output to control the heater
 
 int main(void) {
   // call xTaskCreate on tasks
