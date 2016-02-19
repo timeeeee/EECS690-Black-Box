077f30b4f4415dbc2a112848e91203428fedb77b
DSoliz <zilosgodi@gmail.com>
2016-02-19 16:57:20 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,27 +8,41 @@
 //   FreeRTOS
 
 // Declare globals
-extern double v_temp;  //measured temperature voltage
-extern double dig_temp; // the digital representation of the voltage)
-extern double conv_temp; //the measured temperature in actual degrees (celsius?)
-extern double set_temp;  //the goal temperature
-extern double pid_out;  //the PID output to control the heater
+extern double v_temp;  //!< Measured temperature voltage
+extern double dig_temp; //!< The digital representation of the voltage)
+extern double conv_temp; //!< The measured temperature in actual degrees (celsius?)
+extern double set_temp;  //!< The goal temperature
+extern double pid_out;  //!< The PID output to control the heater
 
 int main(void) {
-  // call xTaskCreate on tasks
+    //!ADC Module task creation.
+    /*!
+      Creates an ADC task to be handled by the freeRTOS task scheduler.
+    */
+    xTaskCreate(ADC_TASK);
+    //! Heater Module task creation.
+    /*!
+      Creates Heater task to be handled by the freeRTOS task scheduler.
+    */
+    xTaskCreate(HEATER_TASK);
+    //! Temperature Convertor task creation.
+    /*!
+      Creates Temperature Converter task to be handled by the freeRTOS task scheduler.
+    */
+    xTaskCreate(TEMP_CONV_TASK);
+    //! System Initilization.
+    /*!
+      Initiliazed the boards system.
+    */
+    SystemInitialize();
+    //! Task Scheduler
+    /*!
+      Initiliazes freeRTOS task scheduler.
+    */
+    vTaskStartScheduler();
 
-  // call init functions
-
-  vTaskStartScheduler();
-
-  while (1) {
-    
+  while (true) {
   }
 }
-
-//   ADC converter
-//   Temperature converter
-//   PID controller
-//   Heater
 
 
