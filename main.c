46ccc9defb44e4e5495cb8a22e454f6adaa606d9
Paul Charles <santosh@ku.edu>
2016-02-20 19:17:37 -0600

    Updated Formatting Changes

--- main.c
+++ main.c
@@ -1,43 +1,51 @@
-// Include things
-//   modules:
-//     ADC converter
-//     PID controller
-//     Heater
+//******************************************************************************
+//! Include things
+//!  modules:
+//!\param     ADC converter
+//!\param     PID controller
+//!\param    Heater
+//!\param   FreeRTOS
+//******************************************************************************
 
-//   FreeRTOS
+//******************************************************************************
+//! Declare globals
+//******************************************************************************
 
-// Declare globals
-extern double v_temp;  //!< Measured temperature voltage
+extern double v_temp;   // Measured temperature voltage
 extern double dig_temp; //!< The digital representation of the voltage)
-extern double conv_temp; //!< The measured temperature in actual degrees (celsius?)
+extern double conv_temp; //!< The measured temperature in actual degrees(celsius)
 extern double set_temp;  //!< The goal temperature
 extern double pid_out;  //!< The PID output to control the heater
 
 int main(void) {
-    //!ADC Module task creation.
-    /*!
-      Creates an ADC task to be handled by the freeRTOS task scheduler.
-    */
+    //
+    //ADC Module task creation.
+    //
+    //  Creates an ADC task to be handled by the freeRTOS task scheduler.
+    //
     xTaskCreate(ADC_TASK);
-    //! Heater Module task creation.
-    /*!
-      Creates Heater task to be handled by the freeRTOS task scheduler.
-    */
+    //
+    // Heater Module task creation.
+    //
+    //  Creates Heater task to be handled by the freeRTOS task scheduler.
+    //
     xTaskCreate(HEATER_TASK);
-    //! Temperature Convertor task creation.
-    /*!
-      Creates Temperature Converter task to be handled by the freeRTOS task scheduler.
-    */
+    // Temperature Convertor task creation.
+    //
+    //  Creates Temperature Converter task to be handled by the freeRTOS task scheduler.
+    //
     xTaskCreate(TEMP_CONV_TASK);
-    //! System Initilization.
-    /*!
-      Initiliazed the boards system.
-    */
+    //
+    // System Initilization.
+    //
+    //  Initiliazed the boards system.
+    //
     SystemInitialize();
-    //! Task Scheduler
-    /*!
-      Initiliazes freeRTOS task scheduler.
-    */
+    //
+    // Task Scheduler
+    //
+    //  Initiliazes freeRTOS task scheduler.
+    //
     vTaskStartScheduler();
 
   while (true) {
