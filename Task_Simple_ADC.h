1688111a52638bcd74f70a9176e7c576edc241da
DSoliz <zilosgodi@gmail.com>
2016-04-09 12:48:45 -0500

    -SImple ADC Module

---
+++ Task_Simple_ADC.h
@@ -0,0 +1,16 @@
+/*
+ * Task_Simple_ADC.h
+ *
+ *  Created on: Apr 9, 2016
+ *      Author: dsolizca
+ */
+
+#ifndef TASKS_TASK_SIMPLE_ADC_H_
+#define TASKS_TASK_SIMPLE_ADC_H_
+
+#include "queue.h"
+
+extern QueueHandle_t myQ;
+
+
+#endif /* TASKS_TASK_SIMPLE_ADC_H_ */
