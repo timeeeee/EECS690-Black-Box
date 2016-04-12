bc94bd1218ee2fd73c2bf158334adab9f9a46fa6
Tim Clark <timeeeee@gmail.com>
2016-04-12 17:14:29 -0500

    update pid.c to use global variables

--- pid.c
+++ pid.c
@@ -1,86 +1,70 @@
-#include <math.h>
-#include <heater.h>
-declare static variables error, integral, previous_error;
+#include "globals.h"
 
-static unsigned long temp1;//temp variable to store the setpoint temp, Ts
-static unsigned long temp2;//temp variable for Tc
-unsigned long Kp=100; //sum of previous error
-unsigned long Ki=100;//
-unsigned long Kd=100;
-unsigned long error=0;
-unsigned long error_2=0;
-unsigned long prev_error=0;
-unsigned long count=0;
-unsigned PID_trigger=0;
+/*
+From globals.h, we get:
+
+  - float conv_temp, the current measured temperature
+  - float set_temp, the current target temperature
+  - unsigned longs Kp, Ki, Kd (declared but not set)
+  - float pid_out, the output variable used to drive the heater
+*/
+
+float dt;
+float error;
+float prev_error;
+float integral;
+int delay_ms = what;
+
+
 void PIDInit()
 {
   integral = 0;
-  previous_error = 0;
+
+  prev_error = 0;
+  
+  Kp = 100;
+  Ki = 100;
+  Kd = 100;
 }
 
-public static double trapez_funct(double x, double err1, int cnt)
+// Calculate the "area" between errors 'err1' and 'err2' over time 'dt'
+static double trapez_funct(float err1, float err2, float dt)
 {
-  unsigned long double y;
-  for(int i=0; i< cnt; i++)
-  {
-    y=(err1/2)*(x + (2*x+err1) + y);
-  }
-  return y;
+  return (err1 + err2) / 2 / dt;
 }
 
 void PIDTask(void *pvParameters)
 {
-  while (true)
-    {
+  while (true) {
+    // PID will take as input current temperature (temp_conv) and target
+    // temperature (temp_set).
+    // It will output a control variable control_variable to drive a PWM
+    // The output is the sum three parts, with coefficients for each:
+    //     Proportional: Kp * error
+    //     Integral: Ki * integral of error so far
+    //     Derivative: Kd * rate of change of error
 
-      temp1=Ts;
-      temp2=Tc;
+    // Get time since last loop
+    dt = 1.0; // fix this
 
+    error = Ts - Tc;
 
+    // Add proportional part
+    pid_out = Kp * error;
 
-      // PID will take as input Tc (temperature converted) and Ts (desired temperature)
-      // It will output a control variable control_variable to drive a PWM
-      // The output is the sum three parts, with coefficients for each:
-      //     Proportional: Kp * error
-      //     Integral: Ki * integral of error so far
-      //     Derivative: Kd * rate of change of error
+    // Update integral and add integral part
+    integral += trapez_funct(err, prev_err, dt);
+    pid_out += Ki * integral;
 
-      error = temp1-temp2; //(  error = Ts - Tc)
-      while (error != 0)
-      {
-        //output heat:
-        PID_trigger=1;
+    // Update derivative part
+    // Wikipedia indicates this is highly prone to noise- we may want to:
+    //   Skip it altogether (sounds like this still gives ok results)
+    //   Use moving-window average as a low-pass filter
+    pid_out += Kd * (error - prev_error) / dt;
 
+    // Save this error for next step
+    prev_err = error;
 
-        control_variable = Kp * error;
-
-        //Perform trapezoidal using integral function
-        integral = trapez_funct(Ki,error,count);
-        control_variable += Ki * integral;
-        control_variable += Kd * (error - previous_error);// what is dt?
-
-        //PID_trigger=Kp*error + Ki*error_2 + Kd*(error-prev_error);
-        prev_error=error;
-        count++;
-      }
-      PID_trigger=0;
-      /*
-      // Proportional
-      control_variable = Kp * error;
-
-      // Integral
-      // Use trapezoidal approximation to cumulatively calculate integral
-      integral += (error + previous_error) / 2 * dt;
-      control_variable += Ki * integral;
-
-      // Derivative
-      // Wikipedia indicates that this is highly prone to noise- we may want to either:
-      //     Skip it altogether
-      //     Use a moving-window average as a make-shift low-pass filter
-      // Approximate graph of error as straight line from last time step to now
-      control_variable += Kd * (error - previous_error) / dt;
-
-      previous_error = error;
-      vTaskDelay();*/
-    }
+    vTaskDelay(delay_ms);
+  }
 }
